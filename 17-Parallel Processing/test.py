4b98e32b83e53dd83c2f52331a3552ce64a94d32
BoozieBoi <michael.heinecke.1337@gmail.com>
2019-09-08 14:43:55 +0200

    materials completed until including section 16

--- 17-Parallel Processing/test.py
+++ 17-Parallel Processing/test.py
@@ -18,18 +18,11 @@ def find_pi(n):
     return pi
 
 if __name__ == '__main__':
-    N = 10**7  # total iterations
+    N = 10**5  # total iterations
+    P = 5      # number of processes
     
-    P = 1      # number of processes
     p = Pool(P)
     print(timeit.timeit(lambda: print(f'{sum(p.map(find_pi, [N//P]*P))/P:0.7f}'), number=10))
     p.close()
     p.join()
     print(f'{N} total iterations with {P} processes')
-    
-    P = 5      # number of processes
-    p = Pool(P)
-    print(timeit.timeit(lambda: print(f'{sum(p.map(find_pi, [N//P]*P))/P:0.7f}'), number=10))
-    p.close()
-    p.join()
-    print(f'{N} total iterations with {P} processes\n')
